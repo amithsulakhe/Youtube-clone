36c74373da4da0a8448717e7cdca9b132eb5a57d
Amith Sulakhe <amithsulakhe729@gmail.com>
2023-10-11 07:47:32 +0530

    updateMYTUBE

--- src/utils/constant.jsx
+++ src/utils/constant.jsx
@@ -1,4 +1,4 @@
-const optionKey="AIzaSyCKtjWJcYawmur3VVUAFk-5vuaZH3tYDhM"
+const optionKey="AIzaSyA8AR1XwAYBIN1VwUq9z5qc_YGHSaBMUMI"
 export const YOUTUBE_API_KEY="https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=25&q=news&type=video&key="+optionKey
 export const GOOGLE_API_KEY="http://suggestqueries.google.com/complete/search?client=firefox&q="
 export const YOUTUBE_SEARCH_API_KEY="https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q="
